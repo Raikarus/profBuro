13ee339e1b670664f542ca1ca92464111b0d7fd1
Raikarus <ivatim32@yandex.ru>
2023-10-14 22:28:31 +0300

    add multiple users

--- main.php
+++ main.php
@@ -63,7 +63,26 @@
 			  <?php
 				$days = array('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота','Воскресенье');
 				$current_date = date('d-m-Y');
+				function random_color() {
+				    $color = '#';
+				    for($i = 0; $i < 6; $i++) {
+				        $color .= dechex(rand(0, 15));
+				    }
+				    return $color;
+				}
 				
+				function get_contrast_color($color) {
+				    $r = hexdec(substr($color, 0, 2));
+				    $g = hexdec(substr($color, 2, 2));
+				    $b = hexdec(substr($color, 4, 2));
+				    $y = 0.2126 * $r + 0.7152 * $g + 0.0722 * $b;
+				    if ($y < 128) {
+				        return '#ffffff'; // контрастный цвет - белый
+				    } else {
+				        return '#000000'; // контрастный цвет - черный
+				    }
+				}
+
 			  	for ($i=14; $i >= 0; $i--) { 
 			  ?>
 				  <div class="day">
@@ -85,13 +104,24 @@
 			  		$grid_column_start = 15-$zap['datedif'];
 			  		$grid_row_start = $zap['start_hour']-7;
 			  		$grid_row_end = $zap['end_hour']-6;
+			  		$background_color = random_color();
+			  		$color = get_contrast_color($background_color);
 
+			  		$query = "SELECT full_name,ROW_NUMBER() OVER (ORDER BY full_name) AS id FROM (SELECT full_name,EXTRACT(DAY FROM AGE(NOW(), shift_date)) AS datedif,EXTRACT(HOUR FROM start_time) as start_hour,EXTRACT(HOUR FROM end_time) AS end_hour FROM users JOIN schedule ON users.id=user_id) as t1 WHERE datedif={$zap['datedif']} AND start_hour={$zap['start_hour']} AND end_hour={$zap['end_hour']}";
 			  		echo "<div class='schedule_item' style='
 			  			grid-column-start: {$grid_column_start};
 			  			grid-row-start: {$grid_row_start};
-			  			grid-row-end: {$grid_row_end}
+			  			grid-row-end: {$grid_row_end};
+			  			background-color: {$background_color};
+			  			color: {$color}
 			  		'>
-			  		{$zap['full_name']}</div>";
+			  		";
+			  		$res2 = pg_query($query);
+
+			  		while ($fio = pg_fetch_assoc($res2)) {
+			  			echo "".$fio['id'].".".$fio['full_name']."<br>";
+			  		}
+			  		echo "</div>";
 			  	}
 			  ?>
 		  </div>

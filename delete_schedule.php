0c47ee3a504a3759fb8a38f10b5fdc0259bff511
Raikarus <ivatim32@yandex.ru>
2023-11-29 19:43:44 +0300

    mobile index style

--- delete_schedule.php
+++ delete_schedule.php
@@ -8,5 +8,5 @@
 	$id = $_POST['schedule_id'];
 	$query = "DELETE FROM schedule WHERE id = {$_POST['schedule_id']}";
 	$res = pg_query($query);
-	header("Location: $path/main.php");
+	header("Location: $path/main.php/?page=moderator");
 ?>

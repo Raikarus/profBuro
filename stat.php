04ce9083a7e2cba9a93428be7626736e93c39925
Raikarus <ivatim32@yandex.ru>
2023-11-29 18:44:42 +0300

    add style to stat

--- stat.php
+++ stat.php
@@ -1,8 +1,10 @@
 <?php
 	$query = "SELECT full_name,SUM(total_time_min) as total FROM users LEFT OUTER JOIN schedule ON users.id=schedule.user_id GROUP BY full_name HAVING SUM(total_time_min)>0 ORDER BY total DESC";
 	$res = pg_query($query);
-
+	echo "<table>";
+	echo "<tr><th>ФИО:</th><th>Минуты:</th></tr>";
 	while ($zap = pg_fetch_assoc($res)) {
-		echo "{$zap['full_name']} - {$zap['total']}<br>";
+		echo "<tr><td>{$zap['full_name']}</td><td>{$zap['total']}</td></tr>";
 		}
+	echo "</table>";	
 ?>
